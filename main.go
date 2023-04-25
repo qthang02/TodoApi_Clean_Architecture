2c3f3b733eea55e15130dff0e2ea612bb73643c5
qthang02 <nguyenquocthang909@gmail.com>
2023-04-25 17:35:12 +0700

    feat: created get item by id

--- main.go
+++ main.go
@@ -7,27 +7,29 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
 type TodoItem struct {
-	Id          int        `json:"id"`
-	Title       string     `json:"title"`
-	Description string     `json:"description"`
-	Status      string     `json:"status"`
-	CreatedAt   *time.Time `json:"created_at"`
-	UpdatedAt   *time.Time `json:"updated_at"`
+	Id          int        `json:"id" gorm:"column:id;"`
+	Title       string     `json:"title" gorm:"column:title;"`
+	Description string     `json:"description" gorm:"column:description;"`
+	Status      string     `json:"status" gorm:"column:status;"`
+	CreatedAt   *time.Time `json:"created_at" gorm:"column:created_at;"`
+	UpdatedAt   *time.Time `json:"updated_at" gorm:"column:updated_at;"`
 }
 
+func (TodoItem) TableName() string { return "todo_items" }
+
 type TodoItemCreation struct {
-	Id          int    `json:"id" gorm:"column:id;"`
+	Id          int    `json:"-" gorm:"column:id;"`
 	Title       string `json:"title" gorm:"column:title;"`
 	Description string `json:"description" gorm:"column:description;"`
-	Status      string `json:"status" gorm:"column:status;"`
 }
 
 func (TodoItemCreation) TableName() string {
-	return "todo_items"
+	return TodoItem{}.TableName()
 }
 
 func main() {
@@ -46,7 +48,7 @@ func main() {
 		{
 			items.POST("", CreateItem(db))
 			items.GET("")
-			items.GET("/:id")
+			items.GET("/:id", GetItem(db))
 			items.PATCH("/:id")
 			items.DELETE("/:id")
 		}
@@ -79,6 +81,38 @@ func CreateItem(db *gorm.DB) func(*gin.Context) {
 
 		c.JSON(http.StatusOK, gin.H{
 			"data": data.Id,
+		})
+
+	}
+}
+
+func GetItem(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data TodoItem
+
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		data.Id = id
+
+		if err := db.Where("id = ?", id).First(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": data,
 		})
 
 	}
