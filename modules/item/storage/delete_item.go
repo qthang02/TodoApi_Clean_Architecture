5ab765d42622ea9a965959d799788e7fd197d50d
qthang02 <nguyenquocthang909@gmail.com>
2023-05-04 22:42:31 +0700

    refactor: update DeleteItem at storage

--- modules/item/storage/delete_item.go
+++ modules/item/storage/delete_item.go
@@ -6,8 +6,15 @@ import (
 )
 
 func (s *sqlStore) DeleteItem(ctx context.Context, cond map[string]interface{}) error {
-	if err := s.db.Table(model.TodoItem{}.TableName()).
-		Where(cond).Updates(map[string]interface{}{"status": "Deleted"}).Error; err != nil {
+
+	deleteStatus := model.ItemStatusDeleted
+
+	if err := s.db.
+		Table(model.TodoItem{}.TableName()).
+		Where(cond).
+		Updates(map[string]interface{}{
+			"status": deleteStatus.String()}).
+		Error; err != nil {
 		return err
 	}
 
